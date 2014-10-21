184e761f29ef08bf7963ba99898fd268b8f05fdb
raielin@gmail.com <raielin@gmail.com>
2014-10-21 17:24:57 -0400

    Ran rake g devise:views

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of your si  te routed with "root"
   # root 'welcome#index'
 
   # Example of regular route:
